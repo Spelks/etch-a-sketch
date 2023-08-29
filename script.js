96688ee4fc900f715bf0e2f7ab1911332f65c328
Alex <131786787+Spelks@users.noreply.github.com>
2023-08-29 11:31:48 +0100

    Add colorFest function for random color button. Adjusted 'colorPick()' function.

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ const drawSection = document.querySelector(".draw-section");
 const colorChoice = document.querySelector("#pen-color");
 const gridBtn = document.querySelector(".grid-btn");
 const gridStatus = document.querySelector(".grid-status");
+const colorfestBtn = document.querySelector(".colorfest-btn");
 
 resetBtn.addEventListener("click", resetGrid);
 slider.addEventListener("input", handleSliderInput);
 eraserBtn.addEventListener("click", toggleEraser);
 gridBtn.addEventListener("click", toggleGrid);
+colorfestBtn.addEventListener("click", colorFest);
 
+//Toggles the eraser effect on or off based on isErasing and eraserBtn status
 function toggleEraser() {
     isErasing = !isErasing;
-    drawSection.classList.toggle("eraser-mode", isErasing);
+    drawSection.classList.toggle("eraser-mode", isErasing); //applies the mouse eraser icon
     eraserBtn.classList.toggle("active", isErasing);
 }
 
@@ -43,6 +46,7 @@ function drawGrid(num) {
 //Pick pen color from pop up
 function colorPick() {
     const penColor = document.querySelectorAll(".grid-item");
+    const colors = ["#FF5733", "#33FF57", "#5733FF", "#FFC300", "#FF33EA", "#33EAFB", "#FF9933", "#33FFD1", "#E533FF", "#FF3333"];
     penColor.forEach((item) => {
         item.addEventListener("pointerdown", ()=> {
             isDrawing = true;
@@ -51,6 +55,9 @@ function colorPick() {
             if (isDrawing === false) return;
             if (isErasing) {
                 item.style.backgroundColor = "";
+              } else if (isColorFest) {
+                const randomColor = colors[Math.floor(Math.random() * colors.length)];
+                item.style.backgroundColor = randomColor;
               } else {
                 item.style.backgroundColor = colorChoice.value;
               }
@@ -74,6 +81,7 @@ function removeGrid() {
     drawSection.textContent = "";
 }
 
+//Grid function to apply or remove 1px border 'grid' based on gridBtn state
 function toggleGrid() {
     const gridBorder = document.querySelectorAll(".grid-item");
     isGrid = !isGrid;
@@ -88,16 +96,19 @@ function toggleGrid() {
     })
 }
 
+function colorFest() {
+    isColorFest = !isColorFest;
+    colorfestBtn.classList.toggle("active", isColorFest);
+}
+
 let isErasing = false; //Set default status of Eraser button
 let isDrawing = false; //Prevents drawing until 'pointerdown' eventListener fires
-let isGrid = !true;
+let isGrid = !true; //Status of grid on the draw area
+let isColorFest = false; //Dormant default status of the multi-color pen tool
 
 output.innerText = `${slider.value} x ${slider.value}`; // Default slider value
-gridStatus.innerText = "Off";
+gridStatus.innerText = "Off"; //global default grid text
 
 drawGrid(parseInt(slider.value)); //Set the initial grid
-
-
-
 
 
